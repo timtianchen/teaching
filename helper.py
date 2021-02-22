be218a65c50ece537f16f8813039c48e647a3a9b
Tim Chen <timchen@Tims-MacBook-Pro-M1.local>
2021-02-22 10:53:50 +0100

    m

--- helper.py
+++ helper.py
@@ -7,18 +7,19 @@ from matplotlib.widgets import Slider, Button, RadioButtons
 from ipywidgets import *
 
 def fun_n(sigma_t, theta):
-    sigma_xxp = 1/2 * (sigma_t[0,0] + sigma_t[1,1]) + \
+    sigma_xp = 1/2 * (sigma_t[0,0] + sigma_t[1,1]) + \
                 1/2 * (sigma_t[0,0] - sigma_t[1,1]) * cos(2 * theta) + \
                 sigma_t[0,1] * sin(2 * theta)
     
-    tau_xyp   = 1/2 * (- sigma_t[0,0] + sigma_t[1,1]) * sin(2 * theta) + \
+    tau_xyp  = 1/2 * (- sigma_t[0,0] + sigma_t[1,1]) * sin(2 * theta) + \
                 sigma_t[0,1] * cos(2 * theta)
     
-    sigma_yyp = 1/2 * (sigma_t[0,0] + sigma_t[1,1]) + \
+    sigma_yp = 1/2 * (sigma_t[0,0] + sigma_t[1,1]) + \
                 1/2 * (- sigma_t[0,0] + sigma_t[1,1]) * cos(2 * theta) - \
                sigma_t[0,1] * sin(2 * theta)
     
-    return [sigma_xxp,tau_xyp,sigma_yyp]
+    sigma_tp  = np.array([[sigma_xp,tau_xyp],[tau_xyp, sigma_yp]])
+    return sigma_tp
     
 def fun_sigma_avg(sigma_t):
     sigma_avg = 1/2*(sigma_t[0,0] + sigma_t[1,1])
@@ -33,4 +34,5 @@ def fun_principal_stress(sigma_avg, radius):
 
 def r(x,d=4):
     return round(x,d)
+
 
